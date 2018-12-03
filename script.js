d8105739d68de37d3699bb66f5cb8179f0688cb1
Riley Conroy <rpconroy@ucar.edu>
2018-12-02 17:49:01 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+////////////////////
+//
+// Adventure Class
+//
+///////////////////
+
+function Adventure()
+{
+    //
+    // Init adventure
+    //
+
+    // Init ActionListeners(scrolldown)
+
+    // Get Screen Dimensions
+    // Create SVG
+    // Create Welcome mesage
+    //
+}
+Adventure.prototype.addName = function(name)
+{
+    this.name = name;
+}
+
+
+///////////////////////////////////
+//                               //
+// Point of interest (POI) Class //
+//                               //
+///////////////////////////////////
+
+// POI constuctor
+function POI()
+{
+}
+
+// Adds the location name
+POI.prototype.addName = function(name)
+{
+    this.name = name;
+}
